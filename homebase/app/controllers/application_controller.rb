f6e8765235b982c838a6e2e46f7e8463d2468056
Edwin Unger <edwin@ungermail.com>
2015-06-13 13:30:29 -0700

    add devise

--- homebase/app/controllers/application_controller.rb
+++ homebase/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :birthdate, :phone, :parent, :image_url) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :current_password, :password, :password_confirmation, :birthdate, :phone, :parent, :image_url) }
+  end
 end
--- homebase/config/routes.rb
+++ homebase/config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :families do
-    resources :users
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'users#show'
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
-  # root 'users#index'
 
 end
