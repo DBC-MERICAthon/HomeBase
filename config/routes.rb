2e21f9af2e043d1c27d537c69bf5d628dc54a00e
Edwin Unger <edwin@ungermail.com>
2015-06-30 14:53:13 -0700

    oauth2 wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+
   get 'home/index'
 
   root 'home#index'
-
 
 end
---
+++ db/migrate/20150630212337_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :uid
+      t.string :name
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :nickname
+      t.datetime :birthdate
+      t.float :lat
+      t.float :lng
+      t.boolean :parent
+      t.string :image_url
+
+      t.timestamps null: false
+    end
+  end
+end
