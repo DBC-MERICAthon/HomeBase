020086b957d5683688093bd9c0c93a7fb1f8d96f
Edwin Unger <edwin@ungermail.com>
2015-06-30 16:08:30 -0700

    heroku FUUUUUU

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+if ENV['RAILS_ENV'] == 'test' || ENV['RAILS_ENV'] == 'test'
+  Dotenv::Railtie.load
+end
 module Homebase
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
