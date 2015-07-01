63efe49d477740c05141d8a458c8ceb96e8c14c4
Edwin Unger <edwin@ungermail.com>
2015-07-01 13:25:38 -0700

    bulbs

--- app/models/lightbulb.rb
+++ app/models/lightbulb.rb
@@ -1,11 +1,15 @@
 class Lightbulb < ActiveRecord::Base
   def self.lights_on(bulb_number)
     bulb = Huey::Bulb.find(bulb_number)
+    p "*" * 100
+    p bulb
     bulb.update(on:true, bri:255)
   end
 
   def self.lights_off(bulb_number)
     bulb = Huey::Bulb.find(bulb_number)
+    p "*" * 100
+    p bulb
     bulb.update(on:false)
   end
 end
