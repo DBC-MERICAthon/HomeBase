614fb45afb1957f7cb27f560cd65118f9d0ec50a
Edwin Unger <edwin@ungermail.com>
2015-07-01 13:36:15 -0700

    fucking lights

--- app/controllers/lightbulbs_controller.rb
+++ app/controllers/lightbulbs_controller.rb
@@ -2,17 +2,11 @@ class LightbulbsController < ApplicationController
 
   def update
     find_lightbulb = current_user.lightbulbs.find_by(params[:bulb_name])
-    p "* " * 100
-    p params[:lightbulb][:state]
-    p "* " * 100
-    p params[:lightbulb][:state]
     if params[:lightbulb][:state] == "true"
       Lightbulb.lights_on(find_lightbulb.bulb_number)
-      p "9" * 100
 
     else
       Lightbulb.lights_off(find_lightbulb.bulb_number)
-      p "k" * 100
     end
     redirect_to user_path
   end
